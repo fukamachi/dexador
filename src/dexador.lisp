79e5c8f1bd69b8d13f46ade601b65206e5e21251
Eitaro Fukamachi <e.arrows@gmail.com>
2015-02-24 11:07:11 +0900

    Add shotcut functions -- get, post, head, put and delete.

--- src/dexador.lisp
+++ src/dexador.lisp
@@ -2,11 +2,48 @@
 (defpackage dexador
   (:nicknames :dex)
   (:use :cl)
+  (:shadow :get
+           :delete)
   #-(and unix (not clisp))
   (:import-from :dexador.backend.usocket
                 :request)
   #+(and unix (not clisp))
   (:import-from :dexador.backend.posix
                 :request)
-  (:export :request))
+  (:export :request
+           :get
+           :post
+           :head
+           :put
+           :delete))
 (in-package :dexador)
+
+(defun get (uri &key (protocol :http/1.1) socket keep-alive)
+  (request uri :method :get
+               :protocol protocol
+               :socket socket
+               :keep-alive keep-alive))
+
+(defun post (uri &key (protocol :http/1.1) socket keep-alive)
+  (request uri :method :post
+               :protocol protocol
+               :socket socket
+               :keep-alive keep-alive))
+
+(defun head (uri &key (protocol :http/1.1) socket)
+  (request uri :method :head
+               :protocol protocol
+               :socket socket
+               :keep-alive nil))
+
+(defun put (uri &key (protocol :http/1.1) socket keep-alive)
+  (request uri :method :put
+               :protocol protocol
+               :socket socket
+               :keep-alive keep-alive))
+
+(defun delete (uri &key (protocol :http/1.1) socket keep-alive)
+  (request uri :method :delete
+               :protocol protocol
+               :socket socket
+               :keep-alive keep-alive))
