594c61ea00118bbefd177bfb5e9bd91ad1fb707b
Eitaro Fukamachi <e.arrows@gmail.com>
2015-03-03 12:45:24 +0900

    Fix and add some tests.

--- t/dexador.lisp
+++ t/dexador.lisp
@@ -16,8 +16,9 @@
        (dex:get "http://localhost:4242/foo"
                 :headers '((:x-foo . "ppp")))
      (is code 200)
-     (is (babel:octets-to-string body) "/foo")
-     (is (gethash "content-length" headers) 4))))
+     (is body "/foo")
+     (is (gethash "content-length" headers) 4)))
+ "normal case")
 
 (test-app
  (lambda (env)
@@ -38,7 +39,7 @@
      (multiple-value-bind (body code headers)
          (dex:get "http://localhost:4242/1")
        (is code 200)
-       (is (babel:octets-to-string body) "OK")
+       (is body "OK")
        (is (gethash "content-length" headers) 2)))
    (subtest "not enough redirect"
      (multiple-value-bind (body code headers)
@@ -56,6 +57,60 @@
      (multiple-value-bind (body code)
          (dex:post "http://localhost:4242/301")
        (declare (ignore body))
-       (is code 302)))))
+       (is code 302))))
+ "redirection")
+
+(test-app
+ (lambda (env)
+   (let ((req (clack.request:make-request env)))
+     (cond
+       ((string= (getf env :path-info) "/upload")
+        (let ((buf (make-array (getf env :)))))
+        `(200 ()
+              (,(with-output-to-string (s)
+                  (alexandria:copy-stream (flex:make-flexi-stream (getf env :raw-body))
+                                          (flex:make-flexi-stream s))))))
+       (T
+        (let ((req (clack.request:make-request env)))
+          `(200 ()
+                (,(with-output-to-string (s)
+                    (loop for (k . v) in (clack.request:body-parameter req)
+                          do (format s "~&~A: ~A~%"
+                                     k
+                                     (if (and (consp v)
+                                              (streamp (car v)))
+                                         (with-output-to-string (s)
+                                           (alexandria:copy-stream (flex:make-flexi-stream (car v)) s))
+                                         v)))))))))))
+ (lambda ()
+   (subtest "content in alist"
+     (multiple-value-bind (body code headers)
+         (dex:post "http://localhost:4242/"
+                   :content '(("name" . "Eitaro")
+                              ("email" . "e.arrows@gmail.com")))
+       (declare (ignore headers))
+       (is code 200)
+       (is body "name: Eitaro
+email: e.arrows@gmail.com
+")))
+   (subtest "multipart"
+     (multiple-value-bind (body code)
+         (dex:post "http://localhost:4242/"
+                   :content `(("title" . "Road to Lisp")
+                              ("body" . ,(asdf:system-relative-pathname :dexador #P"t/data/quote.txt"))))
+       (is code 200)
+       (is body
+           "title: Road to Lisp
+body: \"Within a couple weeks of learning Lisp I found programming in any other language unbearably constraining.\" -- Paul Graham, Road to Lisp
+
+")))
+   (subtest "upload"
+     (multiple-value-bind (body code)
+         (dex:post "http://localhost:4242/upload"
+                   :content (asdf:system-relative-pathname :dexador #P"t/data/quote.txt")
+                   :verbose t)
+       (is code 200)
+       (is body ""))))
+ "POST request")
 
 (finalize)
