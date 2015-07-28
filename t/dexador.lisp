434d0428b202f423dd29a47ad1a3fb900a537056
Eitaro Fukamachi <e.arrows@gmail.com>
2015-07-29 01:13:37 +0900

    Add a test for :want-stream.

--- t/dexador.lisp
+++ t/dexador.lisp
@@ -6,7 +6,7 @@
                 :subtest-app))
 (in-package :dexador-test)
 
-(plan 6)
+(plan 7)
 
 (subtest-app "normal case"
     (lambda (env)
@@ -177,5 +177,16 @@ body: \"Within a couple weeks of learning Lisp I found programming in any other 
       (declare (ignore env))
       '(200 () ("ok")))
   (ok (dex:get "http://localhost:4242/" :verbose t)))
+
+(subtest-app "want-stream"
+    (lambda (env)
+      (declare (ignore env))
+      '(200 (:content-type "text/plain") ("hi")))
+  (let ((body (dex:get "http://localhost:4242/" :want-stream t :keep-alive nil)))
+    (is-type body 'stream)
+    (let ((buf (make-array 2 :element-type '(unsigned-byte 8))))
+      (read-sequence buf body)
+      (is (babel:octets-to-string buf) "hi"))
+    (ignore-errors (close body))))
 
 (finalize)
